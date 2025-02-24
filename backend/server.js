8ef3162985a9bcb992c6a116de6e61a4cbd9131d
Ujjwols <shrestha.ujjwol1@gmail.com>
2025-02-25 01:33:20 +0545

    ....

--- backend/server.js
+++ backend/server.js
@@ -10,12 +10,10 @@ import notificationRoutes from "./routes/notificationRoutes.js"
 import matchRoutes from "./routes/matchRoutes.js"
 import { v2 as cloudinary } from "cloudinary";
 import { app, server } from "./socket/socket.js";
-import job from "./cron/cron.js";
 
 dotenv.config();
 
 connectDB();
-job.start();
 
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
